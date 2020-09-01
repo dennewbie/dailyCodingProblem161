b9562a53ade436c80dd1dccc8bd0d89f8acac12f
DC <caruso.denny@gmail.com>
2020-09-01 13:46:52 +0200

    Initial Commit

---
+++ dailyCodingProblem161/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dailyCodingProblem161
+//
+//  Created by Denny Caruso on 01/09/2020.
+//  Copyright © 2020 Denny Caruso. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
